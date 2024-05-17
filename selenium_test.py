76e3300267957b0e0dc3f08a29fd4196ffe80310
alex-90314 <119699715+alex-90314@users.noreply.github.com>
2024-05-17 11:44:48 -0400

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -5,9 +5,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("http://www.python.org/downloads/")
 assert "Python" in driver.title
-elem = driver.find_element(By.CLASS_NAME, "button")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+elem = driver.find_element(By.CLASS_NAME, "download-buttons")
+elem = driver.find_element(By.PARTIAL_LINK_TEXT)
 assert "No results found." not in driver.page_source
 driver.close()
